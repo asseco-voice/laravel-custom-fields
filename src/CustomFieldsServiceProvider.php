08bc014a06dbd05ac981798d135e4e9f97635a76
Marko Dupor <marko.dupor@asseco-see.hr>
2020-07-30 09:58:20 +0200

    Config change

--- src/CustomFieldsServiceProvider.php
+++ src/CustomFieldsServiceProvider.php
@@ -8,21 +8,21 @@ use Voice\CustomFields\App\Console\Commands\MakeCustomFields;
 class CustomFieldsServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap the application services.
-     */
-    public function boot()
-    {
-        $this->publishes([__DIR__ . '/config/asseco-voice.php' => config_path('asseco-voice.php'),]);
-    }
-
-    /**
      * Register the application services.
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/config/asseco-voice.php', 'asseco-voice');
+        $this->mergeConfigFrom(__DIR__ . '/config/asseco-custom-fields.php', 'asseco-custom-fields');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
         $this->loadRoutesFrom(__DIR__ . '/routes/api.php');
+    }
+
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        $this->publishes([__DIR__ . '/config/asseco-custom-fields.php' => config_path('asseco-custom-fields.php'),]);
 
         $this->registerCreator();
         $this->registerMigrateMakeCommand();
