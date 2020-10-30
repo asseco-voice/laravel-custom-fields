e4b9b8e0b98468093c57bd0a2493c88f75c3b653
Marko Dupor <marko.dupor@gmail.com>
2020-10-30 13:39:41 +0100

    Update migration

--- migrations/2020_05_26_064234_create_custom_fields_table.php
+++ migrations/2020_05_26_064234_create_custom_fields_table.php
@@ -23,7 +23,7 @@ class CreateCustomFieldsTable extends Migration
             $table->morphs('selectable');
             $table->string('model');
             $table->boolean('required')->default(0);
-            $table->foreignId('validation_id')->nullable()->constrained('custom_field_validations')->onDelete('cascade');
+            $table->foreignId('validation_id')->nullable()->constrained('custom_field_validations')->onDelete('set null');
 
             $table->timestamps();
             $table->softDeletes();
