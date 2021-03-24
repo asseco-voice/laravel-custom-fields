8a5c0719cad84d59510def7a6928a56b895bc916
Marko Dupor <Norgul@users.noreply.github.com>
2021-03-24 14:59:23 +0100

    Update CustomFieldUpdateRequest.php (#46)

--- src/App/Http/Requests/CustomFieldUpdateRequest.php
+++ src/App/Http/Requests/CustomFieldUpdateRequest.php
@@ -36,7 +36,7 @@ class CustomFieldUpdateRequest extends FormRequest
             'label'         => 'sometimes|string|max:255',
             'placeholder'   => 'nullable|string',
             'required'      => 'boolean',
-            'validation_id' => 'exists:custom_field_validations',
+            'validation_id' => 'nullable|exists:custom_field_validations',
         ];
 
         return Arr::except($rules, self::LOCKED_FOR_EDITING);
